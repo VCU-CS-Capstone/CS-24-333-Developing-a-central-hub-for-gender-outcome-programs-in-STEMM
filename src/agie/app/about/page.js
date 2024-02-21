2bf799aa09d352ce4cd7beb079599949aa84431b
unknown <bondajs3@vcu.edu>
2024-02-21 13:49:57 -0500

    pushing about page

--- src/agie/app/about/page.js
+++ src/agie/app/about/page.js
@@ -5,9 +5,24 @@ export default function Login() {
     return (
     <div>
         <Navbar />
-      <div className = "text-center text-2xl my-40 ">
-        About Us
+      <div className = "text-center text-7xl mt-40 mb-20 ">
+        About AGIE
       </div>
+        <div className="text-center text-3xl text-bold underline mt-3">
+          Our Story
+        </div>
+        <div className="text-center text-2xl my-3 ml-40 mr-40"> 
+          We aim to create a vibrant community committed to fostering the success and advancement of women in the ever-evolving world of technology. Our mission is to bridge the gender gap in STEMM fields, empower women to thrive in tech careers, and create a supportive network that encourages collaboration and innovation.
+        </div>
+        <div className="text-center text-3xl text-bold underline mt-10">
+          Our Vision
+        </div>
+        <div className="text-center text-2xl my-3 ml-40 mr-40"> 
+        We envision a future where women are not only well-represented in technology but are also leaders, influencers, and pioneers in shaping the industry's landscape. We believe in a world where every woman has equal opportunities to succeed and make impactful contributions to the field.
+        </div>
+
+
+
       <Footer />
     </div>
     );
