34c9e9f6d1c918c375a1bce3e29002736795da69
unknown <bondajs3@vcu.edu>
2024-03-16 13:05:28 -0400

    pushing home page

--- src/agie/app/page.js
+++ src/agie/app/page.js
@@ -15,7 +15,12 @@ export default function Home() {
 
           <div className="infoBox"> 
           
-            <h1>Text here</h1>
+            <h1 className = "text-center text-2xl text-bold mt-3">The Advancing Gender Inclusive Excellence (AGIE) Project</h1>
+            <h2 className = "text-center text-1xl mt-3">Institutional change in higher education regarding diversity, equity and inclusion, tends to be
+            complex, contested, and slow moving. The AGIE Project aims to increase the participation and advancement of diverse women faculty 
+            in the science, technology, engineering, mathematics, and medicine (STEMM) academic and research workforce. This central repository provides data, 
+            tools, programs, and strategies that promote gender equity at the faculty and leadership levels in the STEMM academic and research workforce.</h2>
+          
           </div>
 
           <ImageSlider />
