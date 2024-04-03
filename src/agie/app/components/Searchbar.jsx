457f0228866b8e1ec026808880e74083b165699a
Matt Frayser <mattfrayser@matts-mbp.mynetworksettings.com>
2024-04-03 15:02:35 -0400

    Moved db files

--- src/agie/app/components/Searchbar.jsx
+++ src/agie/app/components/Searchbar.jsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useState } from 'react';
 import Link from "next/link";
-//import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 
 function SearchBar() {
