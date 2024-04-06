c2276ec7ef115bd0ebf564d0fe3600f64a7c9b16
unknown <bondajs3@vcu.edu>
2024-04-06 14:02:12 -0400

    pushing new endpoint for searchbar

--- src/capstone-backend/src/CS_24_333.py
+++ src/capstone-backend/src/CS_24_333.py
@@ -2,41 +2,53 @@
 #| echo: false
 import os
 import sys
+import traceback
 import pandas as pd
 from tabulate import tabulate
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
-from flask import Flask, jsonify, json, logging, request
-from sqlalchemy.exc import OperationalError, ProgrammingError
+from flask import Flask, jsonify, request
+from sqlalchemy.exc import SQLAlchemyError
 from flask_cors import CORS
 
-config_map = {
-    'user':'CS_24_333_USER',
-    'password':'CS_24_333_PASSWORD',
-    'host':'CS_24_333_HOST',
-    'database':'CS_24_333_DBNAME'
-}
-# load and store credentials
+# Initialize app
+app = Flask(__name__)
+CORS(app)
+
+# Retrieve details of accessing database from .env
 load_dotenv()
-config = {}
-for key in config_map.keys():
-    config[key] = os.getenv(config_map[key])
-flag = False
-for param in config.keys():
-    if config[param] is None:
-        flag = True
-        print(f"Missing {config_map[param]} in .env file")
+config = {
+    'user': os.getenv('CS_24_333_USER'),
+    'password': os.getenv('CS_24_333_PASSWORD'),
+    'host': os.getenv('CS_24_333_HOST'),
+    'database': os.getenv('CS_24_333_DBNAME')
+}
 
+# Check for missing config parameters
+for key, value in config.items():
+    if value is None:
+        raise ValueError(f"Missing '{key}' in .env file")
 
-
-engine_uri = f"mysql+pymysql://{config['user']}:{config['password']}@{config['host']}/{config['database']}"
-
-
+# Engine
+engine_uri = f"mysql+pymysql://{config['user']}:{config['password']}@{config['host']}/{config['database']}?connect_timeout=30"
 cnx = create_engine(engine_uri)
 
-def my_sql_wrapper(query):
+# Error handling 
+@app.errorhandler(Exception)
+def handle_exception(e):
+    # Handle SQLAlchemy Errors
+    if isinstance(e, SQLAlchemyError):
+        error_info = str(e.__dict__['orig'])
+        return jsonify({'error': 'Database error', 'details': error_info}), 500
+    # Other Errors
+    else:
+        traceback.print_exc()
+        return jsonify({'error': 'An unexpected error occurred', 'details': str(e)}), 500
+
+
+def my_sql_wrapper(query, params):
     try:
-        df = pd.read_sql(query,cnx)
+        df = pd.read_sql(query, cnx, params=params)
     except Exception as e:
         message = str(e)
         print(f"An error occurred:\n\n{message}\n\nIgnoring and moving on.")
@@ -56,102 +68,83 @@ def my_sql_statement( statement ):
 #    conn = create_engine(engine_uri)
     return result
 
-app = Flask(__name__)
-CORS(app)
 
 @app.route("/hello", methods=["GET"])
 def hello():
     return 'Hello, world!'
 
-#displays all papers in the database
+#displays the papers that fit the inputted category (by categoryName)
+@app.route('/api/paperByCategory', methods=["GET"])
+def api_show_papersbycategoriesName():
+
+    categoryName = request.args.get("categoryName")
+    print(categoryName)
+    
+    if not categoryName:
+        return jsonify({"error": "Missing categoryName query parameter"}), 400
+
+    query = text("""
+        SELECT paper.*
+        FROM paper
+        JOIN paperCategory ON paper.paperPMID = paperCategory.paperPMID
+        JOIN agieCategory ON paperCategory.categoryID = agieCategory.categoryID
+        WHERE agieCategory.categoryName = :categoryName
+    """)
+    
+    df = my_sql_wrapper(query, params={'categoryName': categoryName})
+    
+    categoryData = df.to_dict(orient='records')
+    return jsonify(categoryData)
+
+# Get ALL Categories
+@app.route('/api/categories', methods=["GET"])
+def api_get_categories():
+    try:
+        query = "SELECT DISTINCT categoryName FROM agieCategory"
+        df = my_sql_wrapper(query, params=None)
+        categories = df['categoryName'].tolist()
+        return jsonify(categories)
+    except Exception as e:
+        return jsonify({'error': 'Failed to fetch categories', 'details': str(e)}), 500
+
+# Get ALL papers
 @app.route('/api/paper', methods=["GET"])
 def api_show_paper():
-    df = my_sql_wrapper("select * from paper")
-
-    paper_data = df.to_dict(orient='records')
-
-
-    return jsonify(paper_data)
-
-"""
-#displays data from the paperCategory table with the inputted categoryID
-@app.route('/api/paperofcategory/<int:categoryID>', methods=['GET'])
-def api_show_papersofcategories(categoryID):
     try:
-        category_str = f"'{categoryID}'"
-        query = f"SELECT * FROM paperCategory WHERE categoryID = {categoryID}"
+        query = """
+        SELECT *
+        FROM paper;
+        """
+        df = my_sql_wrapper(query, params=None)
         
-        df = my_sql_wrapper(query)
-        papercategory_data = df.to_dict(orient='records')
+        paper_data = df.to_dict(orient='records')
 
-        return jsonify(papercategory_data)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-"""
+        return jsonify(paper_data)
     
-#displays the papers that fit the inputted category (by categoryID)
-@app.route('/api/paperbycategory/<int:categoryID>', methods=['GET'])
-def api_show_papersbycategoriesID(categoryID):
-    try:
-        
-        query = f"""
-            SELECT paper.* 
-            FROM paper
-            JOIN paperCategory ON paper.paperPMID = paperCategory.paperPMID
-            WHERE paperCategory.categoryID = {categoryID}
-        """
-        
-        df = my_sql_wrapper(query)
-        papercategory_data = df.to_dict(orient='records')
-
-        return jsonify(papercategory_data)
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        print(f"An error occurred: {e}")
+        return jsonify({"error": "Failed to fetch papers"}), 500
 
-#displays the papers that fit the inputted category (by categoryName)
-@app.route('/api/paperbycategory/<string:categoryName>', methods=['GET'])
-def api_show_papersbycategoriesName(categoryName):
+
+#displays the papers that have matching input words to the papers' titles or abstracts
+@app.route('/api/papersbywords', methods=['GET'])
+def api_show_papers_by_words():
     try:
-        
-        query = f"""
-            SELECT paper.* 
-            FROM paper
-            JOIN paperCategory ON paper.paperPMID = paperCategory.paperPMID
-            JOIN agieCategory ON paperCategory.categoryID = agieCategory.categoryID
-            WHERE agieCategory.categoryName = '{categoryName}'
-        """
-        
-        df = my_sql_wrapper(query)
-        papercategory_data = df.to_dict(orient='records')
-
-        return jsonify(papercategory_data)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-#displays the papers that fit the inputted category (by categoryName)
-@app.route('/api/paperbycategory', methods=['GET'])
-def api_show_papersbycategoriesName2():
-    try:
-        #Getting the category name from search bar input
-        categoryName = request.args.get('categoryName')
+        words = request.args.get('words')
 
         query = f"""
-            SELECT paper.* 
+            SELECT * 
             FROM paper
-            JOIN paperCategory ON paper.paperPMID = paperCategory.paperPMID
-            JOIN agieCategory ON paperCategory.categoryID = agieCategory.categoryID
-            WHERE agieCategory.categoryName = '{categoryName}'
+            WHERE title LIKE '%%{words}%%' OR content LIKE '%%{words}%%'
         """
-        
-        df = my_sql_wrapper(query)
-        papercategory_data = df.to_dict(orient='records')
 
-        return jsonify(papercategory_data)
+        df = my_sql_wrapper(query, params={'words': words})
+        papers_data = df.to_dict(orient='records')
+
+        return jsonify(papers_data)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=8080)
