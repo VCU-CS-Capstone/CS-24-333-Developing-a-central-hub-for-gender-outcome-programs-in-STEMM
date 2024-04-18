d04b926202af2ce89a6af08bff1098a643bc5ed1
unknown <bondajs3@vcu.edu>
2024-04-18 01:35:47 -0400

    pushing updated Footer

--- src/agie/app/components/Footer.jsx
+++ src/agie/app/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
 
     <div className=" fixed bottom-0 w-full h-14 bg-black/90">  
         <div className="flex uppercase text-xl font-bold bg-gold p-3" style={{ color: '#CDAE5E' }}> 
-            AGIE PROJECT
+            AGIE Repository
         </div>
     </div>
     )
