291f629a069802dce104017cdcc0897608ad0ff6
unknown <bondajs3@vcu.edu>
2024-04-23 13:10:30 -0400

    updated footer

--- src/agie-repository/components/Footer.js
+++ src/agie-repository/components/Footer.js
@@ -4,9 +4,9 @@ import React from 'react'
 const Footer = () => {
     return (
 
-    <div className="w-full h-14 bg-black/90">  
+    <div className="fixed bottom-0 w-full h-14 bg-black/90">  
         <div className="flex uppercase text-xl font-bold bg-gold p-3" style={{ color: '#CDAE5E' }}> 
-            AGIE PROJECT
+            AGIE Repository
         </div>
     </div>
     )
